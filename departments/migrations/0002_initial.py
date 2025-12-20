e73110fe8a1d5b921e36ef891541996ebbc9b1bf
pycode <devpython549@gmail.com>
2025-12-20 21:05:12 +0530

    fix manager fk constraint for tidb

--- departments/migrations/0002_initial.py
+++ departments/migrations/0002_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='department',
             name='manager',
-            field=models.ForeignKey(blank=True, help_text='Department manager', null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='managed_departments', to='employees.employee'),
+            field=models.ForeignKey(blank=True, db_constraint=False, help_text='Department manager', null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='managed_departments', to='employees.employee'),
         ),
         migrations.AddField(
             model_name='designation',
